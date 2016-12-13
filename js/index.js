04703ae6f0eca1e70591457bc1a2c755dd34f48f
Ostap <ostapukrainian@ukr.net>
2016-12-13 14:45:28 +0000

    end of the commit

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
- function squareEquation(a, b, c, x1, x2) {
+ function squareEquation(a, b, c) {
      var x1;
      var x2;
-     var a = Number(prompt('Enter a:'));
-     var b = Number(prompt('Enter b:'));
-     var c = Number(prompt('Enter c:'));
      var discriminant = b * b - 4 * a * c;
-     var square  = Math.sqrt(discriminant);
+     var square = Math.sqrt(discriminant);
      if (discriminant > 0) {
-         x1 = (-b - square ) / (2 * a);
-         x2 = (-b + square  )/ (2 * a);
+         x1 = (-b - square) / (2 * a);
+         x2 = (-b + square) / (2 * a);
          document.write('Result x1 = ' + x1 + ';', ' Result x2 = ' + x2 + ';');
      }
      else {
-         document.write('No results');
+         return 'No results';
      }
-     document.write('<br />');
-     document.write('Quadratic equation will look like: ' + a + 'x<sup>2</sup>+' + b + 'x+' + c + '=0' + '.');
+     return 'Quadratic equation will look like: ' + a + 'x<sup>2</sup>+' + b + 'x+' + c + '=0' + '.';
  }
- squareEquation();
+ var a = Number(prompt('Enter a:'));
+ var b = Number(prompt('Enter b:'));
+ var c = Number(prompt('Enter c:'));
+ var result = squareEquation(a, b, c);
+ document.write(result);
  
