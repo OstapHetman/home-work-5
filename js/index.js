097d5c8bae7cec053b939c30e4cd8d628959ff5c
Ostap <ostapukrainian@ukr.net>
2016-12-13 14:29:44 +0000

    ( )

--- js/index.js
+++ js/index.js
@@ -7,8 +7,8 @@
      var discriminant = b * b - 4 * a * c;
      var sq = Math.sqrt(discriminant);
      if (discriminant > 0) {
-         x1 = -b - sq / (2 * a);
-         x2 = -b + sq / (2 * a);
+         x1 = (-b - sq) / (2 * a);
+         x2 = (-b + sq )/ (2 * a);
          document.write('Result x1 = ' + x1 + ';', ' Result x2 = ' + x2 + ';');
      }
      else {
