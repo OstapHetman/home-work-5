88ad10a9d3b670b3fa57de558f32f3e7625fc36e
Ostap <ostapukrainian@ukr.net>
2016-12-13 14:27:43 +0000

    2a

--- js/index.js
+++ js/index.js
@@ -1,23 +1,21 @@
- function myFunction(a,b,c,x1,x2) {
-    	var x1;
-    	var x2;
-    	var a = Number(prompt('Enter a:'));
-		var b = Number(prompt('Enter b:'));
-		var c = Number(prompt('Enter c:'));
-        var discriminant = b * b - 4 * a * c;
-        var sq = Math.sqrt(discriminant);
-        if (discriminant > 0)
-        {
-            x1=-b-sq/(2*a);
-            x2=-b+sq/(2*a);
-            document.write('Result x1 = ' + x1+ ';', ' Result x2 = ' +x2 + ';');
-        }
-        else
-        {
-            document.write('No results');
-        }
-        document.write ('<br />');
-        document.write('Quadratic equation will look like: ' + a+'x<sup>2</sup>+'+b+'x+'+c+'=0'+'.');
-    }
-    myFunction();
-  
+ function myFunction(a, b, c, x1, x2) {
+     var x1;
+     var x2;
+     var a = Number(prompt('Enter a:'));
+     var b = Number(prompt('Enter b:'));
+     var c = Number(prompt('Enter c:'));
+     var discriminant = b * b - 4 * a * c;
+     var sq = Math.sqrt(discriminant);
+     if (discriminant > 0) {
+         x1 = -b - sq / (2 * a);
+         x2 = -b + sq / (2 * a);
+         document.write('Result x1 = ' + x1 + ';', ' Result x2 = ' + x2 + ';');
+     }
+     else {
+         document.write('No results');
+     }
+     document.write('<br />');
+     document.write('Quadratic equation will look like: ' + a + 'x<sup>2</sup>+' + b + 'x+' + c + '=0' + '.');
+ }
+ myFunction();
+ 
